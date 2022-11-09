403d8508960d7a4d200a959bec70fcf94631be49
Alejandro Rafael Alfaro Soto <arasman@gmail.com>
2022-11-09 13:43:09 -0500

    How to read args from URL

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { Navigate, useParams } from 'react-router-dom'
+import { getHeroById } from '../helpers';
 
 export const HeroPage = () => {
+  const {id} = useParams();
+  
+  const hero = getHeroById(id);
+  if (!hero) {
+    // return <>404 - Not found</>
+    return <Navigate to="/marvel"/>
+  }
   return (
     <h1>Hero</h1>
   )
