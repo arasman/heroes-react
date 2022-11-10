e95257c474c9640ee22a97bfe1dca926e949a275
Alejandro Rafael Alfaro Soto <arasman@gmail.com>
2022-11-09 22:21:34 -0500

    Creation of the Private Routes concept

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,13 +5,21 @@ import { Route, Routes } from 'react-router-dom'
 import { LoginPage } from '../auth'
 import { HeroesRoutes} from '../heroes'
 import { Navbar } from '../ui'
+import { PrivateRoute } from './PrivateRoute'
 
 export const AppRouter = () => {
   return (
     <>
         <Routes>
             <Route path="login" element={<LoginPage/>}/>
-            <Route path="/*" element={<HeroesRoutes/>}/>
+            <Route 
+              path='/*'
+              element={
+              <PrivateRoute>
+                <HeroesRoutes/>
+              </PrivateRoute>
+              }>
+            </Route>            
         </Routes>
     </>
   )
