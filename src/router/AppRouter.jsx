604f96672a3e2502d822507e78a3e6172ac33583
Alejandro Rafael Alfaro Soto <arasman@gmail.com>
2022-11-09 22:33:55 -0500

    Addition of Public Routes concept for login

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,14 +4,18 @@ import { Route, Routes } from 'react-router-dom'
 
 import { LoginPage } from '../auth'
 import { HeroesRoutes} from '../heroes'
-import { Navbar } from '../ui'
-import { PrivateRoute } from './PrivateRoute'
+import { PrivateRoute, PublicRoute } from './'
 
 export const AppRouter = () => {
   return (
     <>
         <Routes>
-            <Route path="login" element={<LoginPage/>}/>
+            <Route path="login" element={
+              <PublicRoute>
+                <LoginPage/>
+              </PublicRoute>
+            }/>
+
             <Route 
               path='/*'
               element={
